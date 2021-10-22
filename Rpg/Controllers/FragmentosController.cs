ea75e8aa54c55e80e2eb0961d65e00681a2d0efa
Gabriel Teixeira <biel.gt@gmail.com>
2021-10-22 12:31:21 -0300

    Planejamento de transferencia para asp.net

---
+++ Rpg/Controllers/FragmentosController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Rpg.Models;
+
+namespace Rpg.Controllers
+{
+    public class FragmentosController : Controller
+    {
+        [Route("Fragmentos/Index/{id}")]
+        public IActionResult Index([FromRoute] int id)
+        {
+            return View(new fragmentoHistoria
+            {
+                IdFragmentoHistoria = id,
+                TituloFragmento = "Titulo",
+                TextoFragmento = "Texto",
+                Segmentos = new List<fragmentoHistoria>
+                {
+                    new fragmentoHistoria
+                    {
+                        IdFragmentoHistoria = id + 1,
+                        TituloFragmento = $"Titulo {id + 1}",
+                        TextoFragmento = $"Texto {id + 1}",
+                    },
+                    new fragmentoHistoria
+                    {
+                        IdFragmentoHistoria = id + 2,
+                        TituloFragmento = $"Titulo {id + 2}",
+                        TextoFragmento = $"Texto {id + 2}",
+                    }
+                }
+            });
+        }
+
+        [Route("Fragmentos/Sequencia/{id}")]
+        public IActionResult Sequencia([FromRoute]int id)
+        {
+            fragmentoHistoria fragmento = new fragmentoHistoria { IdFragmentoHistoria = id };
+
+            //Buscar do banco pelo Id fragmento e preencher devidos campos
+            fragmentoHistoria resposta = new fragmentoHistoria();//consulta no banco aqui
+
+            fragmento.TituloFragmento = resposta.TituloFragmento;
+            fragmento.TextoFragmento = resposta.TextoFragmento;
+            //Pegar lista de segmentos e adicionar na lista                
+            fragmento.Segmentos = new List<fragmentoHistoria>();
+
+            foreach (var segmento in resposta.Segmentos)
+            {
+                fragmento.Segmentos.Add(segmento);
+            }
+
+            return View(fragmento);
+        }
+    }
+}
---
+++ Rpg/Models/fragmentoHistoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.Models
+{
+    public class fragmentoHistoria
+    {
+        public int IdFragmentoHistoria { get; set; }
+        public string TituloFragmento { get; set; }
+        public string TextoFragmento { get; set; }
+        public List<fragmentoHistoria> Segmentos { get; set; }
+    }
+}
